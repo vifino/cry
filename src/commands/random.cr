74ea5af6797a52bf8a10452188aa9eb480c8b82a
MrRatermat <ratermat@gmail.com>
2015-08-03 01:15:56 +0100

    Fix tabs

--- src/commands/random.cr
+++ src/commands/random.cr
@@ -2,8 +2,8 @@
 require "../parser/commandhelper.cr"
 class RandomCommand
 	def initialize(parser : CommandParser)
-        parser.command "rnd", "prints a random number between 0 and 100" {|a|
-            prng = Random.new
+		parser.command "rnd", "prints a random number between 0 and 100" {|a|
+			prng = Random.new
 			a.output.send prng.rand(101)
 		}
 	end
