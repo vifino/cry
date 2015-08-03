4dfbcedd155cde423fc1b74262ec54aa2e52f6cf
MrRatermat <ratermat@gmail.com>
2015-08-03 01:27:08 +0100

    Moved random command to basics.cr

--- src/commands/basics.cr
+++ src/commands/basics.cr
@@ -59,5 +59,9 @@ class BasicCommands
 				a.output.send "Usage: tr SET1 [SET2]"
 			end
 		}
+        parser.command "rnd", "prints a random number between 0 and 100" {|a|
+			prng = Random.new
+			a.output.send prng.rand(101)
+		}
 	end
 end
