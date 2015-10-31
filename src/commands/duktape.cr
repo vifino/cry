0444446a93761b45058bbd32197a7a4a54fde680
Adrian Pistol <adinator1999@gmail.com>
2015-10-31 01:02:13 +0100

    duktape kinda works. It still has weird issues, but none I can fix, I think.

--- src/commands/duktape.cr
+++ src/commands/duktape.cr
@@ -5,45 +5,100 @@ require "duktape"
 
 class DuktapeCommands
 	def initialize(parser : CommandParser)
-			# @sbx = Duktape::Sandbox.new 500
+			@duk = DuktapeWrapper.new
 
-			#@output :: Channel::Buffered(String)
-
-			#bindings()
+			while true
+				# A bit more testing and stuff.
+				@duk.eval("Math.PI")
+				@duk.eval("Math")
+				break if @duk.eval("'test'") == "test"
+			end
+			@duk.eval!("Math")
 
 			parser.command "duktape", "run javascript using the duktape engine" {|a|
-				str = ""
-				a.args.each {|s| str = str + s + " "}
+				if !a.args.empty?
+					str = a.raw.gsub(/^#{a.cmd} /, "")
+					puts str
+					puts a.raw
 
-				begin
-					run_js(str, a.output)
-				rescue e
-					a.output.send "Error: #{e}"
+					begin
+						a.output.send "#{@duk.eval!(str)}"
+					rescue e
+						a.output.send "Error: #{e}"
+					end
+				else
+					a.output.send "Usage: duktape [js]"
 				end
 			}
 	end
+end
+class DuktapeWrapper
+	def initialize(timeout = 1000)
+		@sbx :: Duktape::Sandbox
+		reset timeout
+	end
 
-	def run_js(str, output)
-		sbx = Duktape::Sandbox.new 500
+	def reset(timeout)
+		@sbx = Duktape::Sandbox.new timeout
 
-		# Sandbox methods and stuff.
-		sbx.push_global_object
-		sbx.push_proc(1) do |ptr| # write
-			env = Duktape::Sandbox.new ptr
-			txt = env.get_string 0
-			if txt.is_a? String
-				output.send txt
-			end
-			env.return 1 # return success
+		while true
+			break if eval("'a'") == "a"
 		end
-		sbx.put_prop_string -2, "write"
 
-		sbx.eval! "
-		function print(txt) {
-			write(txt + '\n')
-		}
+		bindings
+	end
+
+	def bindings
+		#@sbx.push_global_object
+		#@sbx.push_proc(1) do |ptr| # write
+		#	env = Duktape::Sandbox.new ptr
+		#	txt = env.get_string 0
+		#	if txt.is_a? String
+		#		@output.push txt
+		#	end
+		#	env.return 1 # return success
+		#end
+		#@sbx.put_prop_string -2, "write"
+
+		@sbx.eval "
+		print = null
 		"
+	end
 
-		sbx.eval! str
+	def eval(code)
+		r = @sbx.eval code
+		#out = get_global_var "__OUTPUT__"
+		#@sbx.eval! "var __OUTPUT__ = \"\";"
+		#out
+		get_val -1
+	end
+	def eval!(code)
+		r = @sbx.eval! code
+		#out = get_global_var "__OUTPUT__"
+		#@sbx.eval! "var __OUTPUT__ = \"\";"
+		#out
+		get_val -1
+	end
+
+	def get_val(index)
+		case
+		when @sbx.is_string(index)
+			@sbx.get_string(index)
+		when @sbx.is_number(index)
+			@sbx.get_number(index)
+		else
+			@sbx.to_string(index)
+			v = @sbx.get_string(index)
+			@sbx.pop
+			v
+		end
+	end
+
+	def get_global_var(name)
+		@sbx.push_global_object
+		@sbx.push_string name
+		t = get_val -1
+		@sbx.pop
+		t
 	end
 end
